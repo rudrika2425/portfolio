df5949223670ea2a74966234c08c2b08716d1dc8
rudrika <rudrika.raghav2004@gmail.com>
2024-08-24 22:04:22 +0530

    Update index.html, App.js, SkillCard components, and add new images

--- src/App.js
+++ src/App.js
@@ -30,10 +30,46 @@ function App() {
             Portfolio
           </div>
         }
-        projectDesc="Welcome to my portfolio showcasing a collection of dynamic web projects crafted with precision using HTML, CSS, and JavaScript, powered by React.js. Explore how I blend creativity with technical expertise to deliver engaging and responsive web solutions."
+        projectDesc="Welcome to my portfolio, where I showcase a range of dynamic web projects crafted with precision using HTML, CSS, and JavaScript, and powered by React.js. Explore how I blend creativity with technical expertise to deliver engaging and responsive web solutions. From implementing custom animations and routing to integrating Google Sheets and advanced form functionalities, discover how my projects stand out through innovative design and seamless user experiences."
         projectLink="https://github.com/DevanshSahni/Wiggles"
-        deployedProjectLink="https://wiggles.vercel.app"
+        
         projectImg={require('./images/portfolio.png')}
+      />
+      <ProjectCard
+        id = "project"
+        className = "odd"
+        projectTitle={
+          <div style={{
+            background: 'linear-gradient(to right, #DF8908, #B415FF, #F94892)',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            color: 'transparent'
+          }}>
+            Stylish Cart - Ecommerce Website
+          </div>
+        }
+        projectDesc="Stylish Cart is a dynamic e-commerce platform built with the MERN stack, providing a seamless shopping experience. The website features robust functionalities for managing a shopping cart, including the ability to add and remove products with real-time updates to the total sum. Users can easily navigate through the site with a well-implemented routing system and access personalized features through a secure login and signup page. The backend, developed using Node.js and Express, ensures efficient data management and smooth integration with MongoDB, while React handles the frontend interactions, delivering a responsive and engaging user experience."
+        projectLink="https://github.com/DevanshSahni/Wiggles"
+        
+        projectImg={require('./images/shopping.png')}
+      />
+      <ProjectCard
+        id = "project"
+        className = "odd"
+        projectTitle={
+          <div style={{
+            background: 'linear-gradient(to right, #DF8908, #B415FF, #F94892)',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            color: 'transparent'
+          }}>
+            Stylish Cart - Admin Pannel
+          </div>
+        }
+        projectDesc="Stylish Cart is a dynamic e-commerce platform built with the MERN stack, designed to deliver a seamless shopping experience. A key component of the project is the admin panel, which allows for efficient product management through a user-friendly interface. This panel supports the upload of new products and the deletion of existing ones, facilitated by smooth API integrations and effective data management through Node.js and Express. React powers the frontend, ensuring a responsive and engaging user experience, while a well-implemented routing system and secure login/signup page enhance overall site navigation and security."
+        projectLink="https://github.com/DevanshSahni/Wiggles"
+        
+        projectImg={require('./images/admin.png')}
       />
 
       {/* <ProjectCard
